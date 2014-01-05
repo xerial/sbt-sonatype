aed28038fc24c42a505f5e49c5f69507819ca737
Taro L. Saito <leo@xerial.org>
2014-01-05 23:52:14 +0900

    Extract common code into a class

--- src/main/scala/xerial/sbt/Sonatype.scala
+++ src/main/scala/xerial/sbt/Sonatype.scala
@@ -13,6 +13,8 @@ import org.apache.http.auth.{AuthScope, UsernamePasswordCredentials}
 import org.apache.http.impl.client.{DefaultHttpClient, BasicCredentialsProvider}
 import org.apache.http.client.methods.HttpGet
 import java.io.ByteArrayOutputStream
+import scala.xml.XML
+import org.apache.http.client.HttpClient
 
 
 /**
@@ -20,6 +22,7 @@ import java.io.ByteArrayOutputStream
  * @author Taro L. Saito
  */
 object Sonatype extends sbt.Plugin {
+
 
 
   object SonatypeKeys {
@@ -42,37 +45,22 @@ object Sonatype extends sbt.Plugin {
       s.log.info(s"Nexus repository: $repo")
       s.log.info("Listing staging repository...")
 
-
-      val credt : Option[DirectCredentials] = credentials.value.collectFirst{ case d:DirectCredentials if d.host == credentialHost.value => d}
-      if(credt.isEmpty) {
-        s.log.error(s"No credential is found for ${credentialHost.value}")
-        false
-      }
-
-      val user = credt.get.userName
-      val passwd = credt.get.passwd
-
-      val client = new DefaultHttpClient()
-      try {
-        client.getCredentialsProvider.setCredentials(
-          new AuthScope(credt.get.host, AuthScope.ANY_PORT),
-          new UsernamePasswordCredentials(user, passwd)
-        )
-
+      val rest = new RESTAccess(profile.value, credentials.value, credentialHost.value)
+      rest.withHttpClient { client =>
         val req = new HttpGet(s"${repo}/staging/profile_repositories")
         req.addHeader("Content-Type", "application/xml")
         val response = client.execute(req)
         s.log.info(s"Status line: ${response.getStatusLine}")
-        val entity = response.getEntity
-        val len = entity.getContentLength
-        val b = new ByteArrayOutputStream()
-        entity.writeTo(b)
-        val content = b.toString("UTF-8")
-        s.log.info(content)
-        response.toString
+        val profileRepositoriesXML = XML.load(response.getEntity.getContent)
+        val profiles = for(p <- profileRepositoriesXML \\ "stagingProfileRepository") yield {
+          StagingProfile(
+            (p \ "profileId").text,
+            (p \ "profileName").text,
+            (p \ "type").text,
+            (p \ "repositoryId").text)
+        }
+        s.log.info(s"profiles:\n${profiles.mkString("\n")}")
       }
-      finally
-        client.getConnectionManager.shutdown()
     },
     close := {
       true
@@ -82,7 +70,36 @@ object Sonatype extends sbt.Plugin {
     }
   )
 
+
+
+  case class StagingProfile(profileId:String, profileName:String, stagingType:String, repositoryId:String)
+
+
   private def repoBase(url:String) = if(url.endsWith("/")) url.dropRight(1) else url
+
+  class RESTAccess(profileName:String, cred:Seq[Credentials], credentialHost:String) {
+    def withHttpClient[U](body: HttpClient => U) : U = {
+      val credt : Option[DirectCredentials] = cred.collectFirst{ case d:DirectCredentials if d.host == credentialHost => d}
+      if(credt.isEmpty)
+        throw new IllegalStateException(s"No credential is found for ${credentialHost}")
+
+      val client = new DefaultHttpClient()
+      try {
+        val user = credt.get.userName
+        val passwd = credt.get.passwd
+        client.getCredentialsProvider.setCredentials(
+          new AuthScope(credt.get.host, AuthScope.ANY_PORT),
+          new UsernamePasswordCredentials(user, passwd)
+        )
+        body(client)
+      }
+      finally
+        client.getConnectionManager.shutdown()
+
+    }
+  }
+
+
 
 }
 
